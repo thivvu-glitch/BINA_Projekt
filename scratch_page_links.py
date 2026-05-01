2dc998f08410967648ccd7e91ae541057a0c0102
Thivvirthan <thivvirthan.krishnakumar@students.bfh.ch>
2026-05-01 22:11:41 +0200

    Seite korr gelöscht

--- scratch_page_links.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.sidebar.page_link("scratch_page_links.py", label="Zeit & Liga", icon="1️⃣")
-st.sidebar.page_link("scratch_page_links.py", label="Karten", icon="2️⃣")
-
-st.write("Current page content")
